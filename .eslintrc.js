4221f0ee336e52a78755ea68c4687c379d3af076
Lexxby <iplexx@bk.ru>
2021-12-14 17:20:30 +0300

    add filter to app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: ['plugin:react/recommended', 'standard'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 13,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react'],
   rules: {
     indent: 'off',
     semi: [2, 'always'],
-    'space-before-function-paren': ['error', 'never'],
-    'comma-dangle': ['error', 'never'],
-  },
+    'space-before-function-paren': ['error', { anonymous: 'always', named: 'never' }],
+    'comma-dangle': ['error', 'never']
+  }
 };
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,42 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { paginate } from '../utils/paginate';
 import Pagination from './pagination';
 import User from './user';
 import PropTypes from 'prop-types';
+import GroupList from './groupList';
+import SearchStatus from './searchStatus';
+import api from '../api';
 
 const Users = ({ users, ...rest }) => {
-  const count = users.length;
   const pageSize = 4;
+  const [selectedProf, setSelectedProf] = useState();
+  const [professions, setProfessions] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    api.professions.fetchAll().then((data) => setProfessions(data));
+  }, []);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [selectedProf]);
+
+  const handleProfessionSelect = (item) => {
+    setSelectedProf(item);
+  };
+
   const handlePageChange = (pageIndex) => {
     setCurrentPage(pageIndex);
   };
-  const userCrop = paginate(users, currentPage, pageSize);
+  const filteredUsers = selectedProf ? users.filter((user) => user.profession === selectedProf) : users;
+  const count = filteredUsers.length;
+  const userCrop = paginate(filteredUsers, currentPage, pageSize);
+
+  const clearFilter = () => {
+    setSelectedProf(setCurrentPage(1));
+  };
 
   return (
-    <>
-      {count > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Имя</th>
-              <th>Качества</th>
-              <th>Провфессия</th>
-              <th>Встретился, раз</th>
-              <th>Оценка</th>
-              <th>Избранное</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {userCrop.map((user) => (
-              <User {...rest} {...user} key={user._id} />
-            ))}
-          </tbody>
-        </table>
+    <div className="d-flex">
+      {professions && (
+        <div className="d-flex flex-column flex-shrink-0 p-3">
+          <GroupList selectedItem={selectedProf} items={professions} onItemSelect={handleProfessionSelect} />
+          <button className="btn btn-secondary mt-2" onClick={clearFilter}>
+            Очистить
+          </button>
+        </div>
       )}
-      <Pagination itemsCount={count} pageSize={pageSize} currentPage={currentPage} onPageChange={handlePageChange} />
-    </>
+      <div className="d-flex flex-column">
+        <SearchStatus length={count} />
+        {count > 0 && (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Имя</th>
+                <th>Качества</th>
+                <th>Провфессия</th>
+                <th>Встретился, раз</th>
+                <th>Оценка</th>
+                <th>Избранное</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {userCrop.map((user) => (
+                <User {...rest} {...user} key={user._id} />
+              ))}
+            </tbody>
+          </table>
+        )}
+        <div className="d-flex justify-content-center">
+          <Pagination itemsCount={count} pageSize={pageSize} currentPage={currentPage} onPageChange={handlePageChange} />
+        </div>
+      </div>
+    </div>
   );
 };
 
